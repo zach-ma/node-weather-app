6eb8b2818e411a350e9c6052bd2d273b5ba1c646
zach-ma <mjx.ma@icloud.com>
2022-01-12 10:40:50 -0800

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,16 +13,14 @@ weatherForm.addEventListener("submit", (e) => {
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
 
-  fetch("http://localhost:3000/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          return (messageOne.textContent = data.error);
-        }
-        messageOne.textContent = data.place;
-        messageTwo.textContent = data.weather_descriptions;
-      });
-    }
-  );
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        return (messageOne.textContent = data.error);
+      }
+      messageOne.textContent = data.place;
+      messageTwo.textContent = data.weather_descriptions;
+    });
+  });
   console.log("testing");
 });
